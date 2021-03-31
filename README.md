f6ec77a839451e3314e1614e399d376299252d68
Nico Salinas <salinas.nsalinas@gmail.com>
2021-03-31 18:00:27 -0500

    Created input elements

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Adultz Bops
+6.859 Interactive Data Visualization
 
-Shannen Wu ([shannen@mit.edu](shannen@mit.edu)), Jessica Yin ([yinj@mit.edu](yinj@mit.edu)), Nico Salinas ([nsalinas@mit.edu](nsalinas@mit.edu))
+Team Members: Shannen Wu ([shannen@mit.edu](shannen@mit.edu)), Jessica Yin ([yinj@mit.edu](yinj@mit.edu)), Nico Salinas ([nsalinas@mit.edu](nsalinas@mit.edu))
 
 Dataset URL: https://github.com/the-pudding/data/tree/master/kidz-bop
--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async () => {
   console.log('Done loading DOM...')
-})
+
+  const csvData = await d3.csv("https://raw.githubusercontent.com/the-pudding/data/master/kidz-bop/KB_censored-lyrics.csv");
+});
+
+
+loadWordCloud = () => {
+  
+  const height = 400;
+  const width = 400;
+  const margin = ({
+      top: 10,
+      right: 10,
+      bottom: 20,
+      left: 20
+  });
+
+}
