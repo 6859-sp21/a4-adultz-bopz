e37ce8eabad2f4d0f57fb589443813134678b4f2
Jessica Yin <yinj@mit.edu>
2021-04-04 11:05:52 -0400

    added glow effect + zoom to artist

--- src/Bubbles.js
+++ src/Bubbles.js
@@ -2,16 +2,19 @@ import React, { useRef, useState, useEffect } from "react";
 import * as d3 from "d3";
 import { genNestedData } from "./utils/data-transform";
 
+// svg colors + dims
 const width = 500;
 const height = 500;
-
-const backgroundColor = ['', '#183446', '#5C374C', '#90132E'];
-const backgroundColorHover = ['', '#24506A' ,'#985277', '#B51739']; 
-
-const strokeColor = ["", "#0090C1", "#CE6A85", "#6C0E23"];
-const strokeColorHover = ['', '#0090C1', '#CE6A85', '#6C0E23'];
-
-//d3.scaleSequential(d3.interpolatePlasma).domain([0, 3]);
+const scale1 = d3
+  .scaleSequential(d3.interpolate("#00875A", "#ABF5D1"))
+  .domain([0, 30]);
+const scale2 = d3
+  .scaleSequential(d3.interpolate("#00B8D9", "#B3F5FF"))
+  .domain([2, 8]);
+const scale3 = d3
+  .scaleSequential(d3.interpolate("#C054BE", "#E1C7E0"))
+  .domain([0, 8]);
+const color = [null, scale1, scale2, scale3];
 
 const pack = (data) =>
   d3.pack().size([width, height]).padding(3)(
@@ -21,15 +24,75 @@ const pack = (data) =>
       .sort((a, b) => b.count - a.count)
   );
 
-const Bubbles = () => {
-  const [data, setData] = useState(null);
+let svg, view, label, node, focus;
+
+const Bubbles = ({ songOrArtist }) => {
+  // const [data, setData] = useState(null);
+  const [root, setRoot] = useState(null);
 
   const d3Container = useRef(null);
+
+  const shouldShowLabel = (d) => {
+    return (d.parent === root && d.r > d.data.name.length * 2) || d.depth > 1;
+  };
+
+  const zoomTo = (v) => {
+    const k = width / v[2];
+    view = v;
+    label.attr("transform", (d) => {
+      return `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`;
+    });
+    node.attr(
+      "transform",
+      (d) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
+    );
+    node.attr("r", (d) => d.r * k);
+  };
+
+  const zoom = (event, d) => {
+    focus = d;
+    console.log(focus);
+
+    const transition = svg
+      .transition()
+      .duration(event.altKey ? 7500 : 750)
+      .tween("zoom", (d) => {
+        const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+        return (t) => zoomTo(i(t));
+      });
+
+    node
+      .filter(function (d) {
+        return d.parent === focus || this.style.display === "inline";
+      })
+      .transition(transition)
+      .style("fill-opacity", (d) => (d.parent === focus ? 1 : 0))
+      .on("start", function (d) {
+        if (d.parent === focus) this.style.display = "inline";
+      })
+      .on("end", function (d) {
+        if (d.parent !== focus) this.style.display = "none";
+      });
+
+    label
+      .filter(function (d) {
+        return d.parent === focus || this.style.display === "inline";
+      })
+      .transition(transition)
+      .style("fill-opacity", (d) => (d.parent === focus ? 1 : 0))
+      .on("start", function (d) {
+        if (shouldShowLabel(d)) this.style.display = "inline";
+      })
+      .on("end", function (d) {
+        if (d.parent !== focus) this.style.display = "none";
+      });
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await genNestedData();
-      setData(res);
+      const input = { name: "artists", children: res };
+      setRoot(pack(input));
     };
     fetchData();
   }, []);
@@ -37,39 +100,46 @@ const Bubbles = () => {
   /* The useEffect Hook is for running side effects outside of React,
        for instance inserting elements into the DOM using D3 */
   useEffect(() => {
-    if (data && d3Container.current) {
-      const input = { name: "artists", children: data };
-      const root = pack(input);
-      let focus = root;
-      let view;
+    if (root && d3Container.current) {
+      focus = root;
 
-      const svg = d3
+      svg = d3
         .select(d3Container.current)
         .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
         .style("display", "block")
+        .style("height", "80vh")
         .style("margin", "0 -14px")
         .attr("text-anchor", "middle")
         .style("cursor", "pointer")
         .on("click", (event) => zoom(event, root));
 
-      const node = svg
+      // glow
+      const defs = svg.append("defs");
+      const filter = defs.append("filter").attr("id", "glow");
+      filter
+        .append("feGaussianBlur")
+        .attr("stdDeviation", "2.5")
+        .attr("result", "coloredBlur");
+      const feMerge = filter.append("feMerge");
+      feMerge.append("feMergeNode").attr("in", "coloredBlur");
+      feMerge.append("feMergeNode").attr("in", "SourceGraphic");
+
+      node = svg
         .append("g")
         .selectAll("circle")
         .data(root.descendants().slice(1))
         .join("circle")
-        .attr("fill", (d) => backgroundColor[d.depth])
-        .attr("stroke", (d) => strokeColor[d.depth])
+        .attr("stroke", (d) => color[d.depth](d.r))
+        .attr("fill", "transparent")
         .style("display", (d) => (d.parent === root ? "inline" : "none"))
         .on("mouseover", (e, d) => {
           d3.select(e.target)
-              .attr("fill", backgroundColorHover[d.depth])
-              .attr("stroke", strokeColorHover[d.depth])
+            .attr("stroke-width", 4)
+            .style("filter", "url(#glow)");
           d3.select("text");
         })
-        .on("mouseout", (e, d) => {
-          d3.select(e.target)
-              .attr("fill", backgroundColor[d.depth])
-              .attr("stroke", strokeColor[d.depth])
+        .on("mouseout", (e) => {
+          d3.select(e.target).attr("stroke-width", 1).style("filter", null);
         })
         .on("click", (event, d) => {
           if (focus !== d) {
@@ -78,84 +148,41 @@ const Bubbles = () => {
           }
         });
 
-      const shouldShowLabel = (d) => {
-        return (d.parent === root && d.r > 20) || d.depth > 1;
-      };
-
-      const label = svg
+      label = svg
         .append("g")
-        .style("font", "5px Lato")
+        .style("font", "8px Lato")
         .attr("pointer-events", "none")
         .attr("text-anchor", "middle")
         .selectAll("text")
         .data(root.descendants())
         .join("text")
+        .style("maxWidth", (d) => d.r)
         .style("fill", "white")
         .style("fill-opacity", (d) => (d.parent === root ? 1 : 0))
         .style("display", (d) => (shouldShowLabel(d) ? "inline" : "none"))
         .text((d) => (!d.children ? d.data.songName : d.data.name));
 
-      const zoomTo = (v, t) => {
-        const k = width / v[2];
-        view = v;
-        label.attr("transform", (d) => {
-          return `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`;
-        });
-        node.attr(
-          "transform",
-          (d) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
-        );
-        node.attr("r", (d) => d.r * k);
-      };
-
-      zoomTo([root.x, root.y, root.r * 2], 1);
-
-      const zoom = (event, d) => {
-        focus = d;
-        console.log(focus);
-
-        const transition = svg
-          .transition()
-          .duration(event.altKey ? 7500 : 750)
-          .tween("zoom", (d) => {
-            const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-            return (t) => zoomTo(i(t), t);
-          });
-
-        node
-          .filter(function (d) {
-            return d.parent === focus || this.style.display === "inline";
-          })
-          .transition(transition)
-          .style("fill-opacity", (d) => (d.parent === focus ? 1 : 0))
-          .on("start", function (d) {
-            if (d.parent === focus) this.style.display = "inline";
-          })
-          .on("end", function (d) {
-            if (d.parent !== focus) this.style.display = "none";
-          });
-
-        label
-          .filter(function (d) {
-            return d.parent === focus || this.style.display === "inline";
-          })
-          .transition(transition)
-          .style("fill-opacity", (d) => (d.parent === focus ? 1 : 0))
-          .on("start", function (d) {
-            if (shouldShowLabel(d)) this.style.display = "inline";
-          })
-          .on("end", function (d) {
-            if (d.parent !== focus) this.style.display = "none";
-          });
-      };
-
+      zoomTo([root.x, root.y, root.r * 2]);
       document.addEventListener("keydown", (e) => {
         if (e.key === "Escape") {
           zoom(e, root);
         }
       });
     }
-  }, [data]);
+  }, [root]);
+
+  useEffect(() => {
+    console.log("selected", songOrArtist, root);
+    if (songOrArtist && root) {
+      const newFocus = root
+        .descendants()
+        .filter((d) => d.data.name === songOrArtist.value)[0];
+      console.log("filtered", newFocus);
+      if (newFocus) {
+        zoom({}, newFocus);
+      }
+    }
+  }, [songOrArtist]);
 
   return (
     <svg
